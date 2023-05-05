a65a1ea8036b61b47dc8d9f3131fbf8640b762d7
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-05-05 19:48:13 -0400

    Inicializado a criação do componente Card e List

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import List from '../List/index'
 
 import { Container } from './styles';
 
 function Board() {
   return (
     <Container>
-      
+      <List />
+      <List />
+      <List />
+      <List />
     </Container>
   );
 }
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Label } from './styles';
 
 function Card() {
   return (
     <Container>
-      
+      <header>
+        <Label color="#7159C1" />
+      </header>
+      <p>Fazer migração completa de servidor</p>
+      <img src='https://avatars.githubusercontent.com/u/40473246?v=4' alt='' />
     </Container>
   );
 }
