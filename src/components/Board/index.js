f5e7899a52db26ad7dce242a93397232596b2ae6
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-05-06 14:21:48 -0400

    Adicionado contextAPI do React para monitorar o estado da lista e do movimento dos cards

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import List from "../List/index"
 import { loadLists } from "../../services/api";
-
+import BoardContext from "./context";
 import { Container } from "./styles";
 
-const lists = loadLists();
+const data = loadLists();
 
 function Board() {
+
+  const [lists, setLists] = useState(data);
+  function move(from, to) {
+    console.log(from, to);
+  }
+
   return (
-    <Container>
-      {lists.map(list => <List key={list.title} data={list} /> )}
-    </Container>
+    <BoardContext.Provider value={{ lists, move }}>
+      <Container>
+        {lists.map(list => <List key={list.title} data={list} /> )}
+      </Container>
+    </BoardContext.Provider>
   );
 }
 
