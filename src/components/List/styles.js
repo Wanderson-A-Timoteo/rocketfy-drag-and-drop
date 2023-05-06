347b17855a9f5a538c2b620229b8530374615457
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-05-06 10:34:01 -0400

    Adicionado estilo na lista

--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -18,4 +18,34 @@ export const Container = styled.div`
   flex-basis: 320px; 
 
 */
+
+  & + div {
+    border-left: 1px solid rgba(0, 0, 0, 0.05);
+  }
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 42px;
+  }
+
+  h2 {
+    font-weight: 500;
+    font-size: 16px;
+    padding: 0 10px;
+  }
+
+  button {
+    width: 42px;
+    height: 42px;
+    border-radius: 18px;
+    background-color: #3B5BFD;
+    border: 0;
+    cursor: pointer;
+  }
+
+  ul {
+    margin-top: 30px;
+  }
 `;
